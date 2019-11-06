b52d753af6e1e008e508fbb19febe4288b2153ab
shbarman <barman.s@husky.neu.edu>
2019-11-05 18:40:34 -0800

    Provision

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -6,8 +6,3 @@ sudo ./shutdown.sh
 sudo rm -rf /opt/tomcat/logs/catalina*
 sudo rm -rf /opt/tomcat/logs/*.log
 sudo rm -rf /opt/tomcat/logs/*.txt
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
-    -a fetch-config \
-    -m ec2 \
-    -c file:/opt/cloudwatch-config.json \
-    -s
--- infrastructure/aws/codedeploy/applicationstart.sh
+++ infrastructure/aws/codedeploy/applicationstart.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -xe
 cd /opt/tomcat/bin
 sudo ./startup.sh
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl  -a fetch-config  -m ec2  -c file:/opt/cloudwatch-config.json  -s
